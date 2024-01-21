5cc64390c6911b016c15d139e3b2d048e74b03ef
Daniel Saisani <daniel@tryterra.co>
2024-01-21 17:21:45 +0000

    fix: exception

--- incubator/__init__.py
+++ incubator/__init__.py
@@ -2,11 +2,9 @@ import os
 
 if not os.path.exists('final-videos'):
     os.mkdir('final-videos')
-    raise Exception('Need to have a folder named final-videos!')
 
 if not os.path.exists('raw-videos'):
     os.mkdir('raw-videos')
-    raise Exception('Need to have folder named raw-videos!')
 
 if not os.path.exists("temp-assets"):
     os.mkdir("temp-assets")
