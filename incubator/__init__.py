12440b194ae8c76ef31c3e46d4aaea0a82df1bd6
Daniel Saisani <daniel@tryterra.co>
2024-01-27 18:12:16 +0000

    temp

--- incubator/__init__.py
+++ incubator/__init__.py
@@ -3,7 +3,10 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
-options.binary_location = "/usr/bin/"  # Adjust the path to where Chrome is installed
+options.binary_location = "/var/lib/dpkg/info/google-chrome-stable.postrm"  # Adjust the path to where Chrome is installed
+# /var/lib/dpkg/info/google-chrome-stable.postrm
+# /var/lib/dpkg/info/google-chrome-stable.list
+# /app/google-chrome-stable_current_amd64.deb
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 if not os.path.exists('final-videos'):
