9a060f886a1a9597190b859fdf7dbb3f36bc03f6
Daniel Saisani <47062269+DxnielKS@users.noreply.github.com>
2024-05-12 11:17:39 +0100

    Update dbm.py

--- incubator/database/dbm.py
+++ incubator/database/dbm.py
@@ -1,6 +1,5 @@
 from incubator.utils.story_id import generate_unique_id
 from incubator.utils.di import inject_supabase_client
-import uuid
 import datetime
 import logging
 
@@ -9,16 +8,14 @@ _LOGGER = logging.getLogger('incubator.database')
 
 def check_if_story_posted(story, supabase_client=inject_supabase_client()) -> bool:
     hashed = generate_unique_id(story)
-    return False
     data = supabase_client.table('videos_posted').select("*").eq('video_hash', hashed).execute()
-    # return len(data.data) > 0
+    return len(data.data) > 0
 
 
 def log_story_posted(title, story, supabase_client=inject_supabase_client()) -> bool:
     hashed = generate_unique_id(story)
     try:
         supabase_client.table('videos_posted').insert({
-            "id": str(uuid.uuid4()),
             "video_hash": hashed,
             "title": title,
             'story': story,
@@ -29,20 +26,15 @@ def log_story_posted(title, story, supabase_client=inject_supabase_client()) -> 
         return False
     return True
 
-def check_if_short_posted(story, supabase_client=inject_supabase_client()) -> bool:
-    hashed = generate_unique_id(story)
-    data = supabase_client.table('videos_posted').select("*").eq('video_hash', hashed).execute()
+def check_if_short_posted(url, supabase_client=inject_supabase_client()) -> bool:
+    data = supabase_client.table('shorts_posted').select("*").eq('short_url', url).execute()
     return len(data.data) > 0
 
 
-def log_short_posted(title, story, supabase_client=inject_supabase_client()) -> bool:
-    hashed = generate_unique_id(story)
+def log_short_posted(url, supabase_client=inject_supabase_client()) -> bool:
     try:
-        supabase_client.table('videos_posted').insert({
-            "id": str(uuid.uuid4()),
-            "video_hash": hashed,
-            "title": title,
-            'story': story,
+        supabase_client.table('shorts_posted').insert({
+            "short_url": url,
             'created_at': datetime.datetime.utcnow().isoformat()
         }).execute()
     except Exception as e:
